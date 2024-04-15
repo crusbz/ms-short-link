536a493bb548978ad8755808871c2203a7750c50
cruzbs <joaocruzbs@gmail.com>
2024-04-14 22:57:14 -0300

    feat: create app module

---
+++ app-microservice/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataOptions } from './config/typeorm.config';
+import { ShortLinkModule } from './short-link/short-link.module';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(dataOptions),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    UserModule,
+    ShortLinkModule,
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
