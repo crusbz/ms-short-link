f6fbbb064592086360cbaf3e290c2c34a5b3b478
crusbz <joaocruzbs@gmail.com>
2024-04-15 14:09:04 -0300

    feat: add validation email in dto

--- app-microservice/src/user/dto/create-user.dto.ts
+++ app-microservice/src/user/dto/create-user.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
   name: string;
-  @IsString()
+  @IsEmail()
   @IsNotEmpty()
   @ApiProperty()
   email: string;
