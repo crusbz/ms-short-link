7aadb3ea816435a94e0b66bf0fcf616da98c166b
cruzbs <joaocruzbs@gmail.com>
2024-04-14 22:53:16 -0300

    fix: change url to env dburl

--- app-microservice/src/config/typeorm.config.ts
+++ app-microservice/src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ import { UserModel } from 'src/user/db/typeorm/models/user.model';
 import { ShortLinkModel } from 'src/short-link/db/typeorm/models/short-link.model';
 
 export const dataOptions: DataSourceOptions = {
-  url: 'postgres://vyqchvhn:u3N1tsiYDUoTOsHSEiNAZ7ALtb3E9EcC@raja.db.elephantsql.com/vyqchvhn',
+  url: process.env.DB_URL,
   type: 'postgres',
   migrations: [__dirname + '/migrations/{*.js,*.ts}'],
   entities: [UserModel, ShortLinkModel],
